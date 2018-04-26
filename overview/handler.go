0c7ceedc0807a04189d12038a71824da15b63091
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-04-26 10:13:53 +0100

    Add overview function
    Overview renders list-functions via HTML using go
    templates and jQuery for use with OpenFaaS Cloud
    statuses via GitHub
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

---
+++ overview/handler.go
@@ -0,0 +1,46 @@
+package function
+
+import (
+	"bytes"
+	"html/template"
+	"log"
+	"net/url"
+	"os"
+)
+
+type UserData struct {
+	User      string
+	PublicURL string
+	PrettyURL string
+}
+
+// Handle a serverless request
+func Handle(req []byte) string {
+	tmpl, err := template.ParseFiles("index.html")
+
+	if err != nil {
+		log.Panic("Error loading template file: ", err)
+	}
+
+	userData1 := UserData{
+		PublicURL: os.Getenv("public_url"),
+		PrettyURL: os.Getenv("pretty_url"),
+	}
+
+	vals, _ := url.ParseQuery(os.Getenv("Http_Query"))
+
+	user := vals.Get("user")
+
+	userData1.User = user
+	log.Println("User: ", user)
+
+	var tpl bytes.Buffer
+
+	err = tmpl.Execute(&tpl, userData1)
+
+	if err != nil {
+		log.Panic("Error executing template: ", err)
+	}
+
+	return tpl.String()
+}
